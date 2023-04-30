1321ab9af0b02fd2a2983f2f74b8f1a25b2d2407
aquino062 <willian_wjunior@hotmail.com>
2023-04-30 20:34:07 -0300

    2.0

--- exercicios/exercicio-04/src/main/java/org/example/Elevador.java
+++ exercicios/exercicio-04/src/main/java/org/example/Elevador.java
@@ -1,46 +1,57 @@
 package org.example;
 
 public class Elevador {
-    int andarAtual = 0;
+    int andarAtual;
     int totalAndares;
     int capacidadeElevador;
     int pessoasNoElevador;
 
-    public Elevador(int andarAtual, int totalAndares, int capacidadeElevador, int pessoasNoElevador) {
-        this.andarAtual = andarAtual;
+    public Elevador(int totalAndares, int capacidadeElevador) {
         this.totalAndares = totalAndares;
         this.capacidadeElevador = capacidadeElevador;
-        this.pessoasNoElevador = pessoasNoElevador;
+        this.andarAtual = 0;
+        this.pessoasNoElevador = 0;
     }
 
 
-    public void entra( int total){
-        if(total > capacidadeElevador || pessoasNoElevador > capacidadeElevador){
+    public void entra(int total) {
+        if ((total + this.pessoasNoElevador) > this.capacidadeElevador) {
             System.out.println("capacidade não suportada");
-        }else{
-            pessoasNoElevador += total;
-            capacidadeElevador += total;
+        } else {
+            this.pessoasNoElevador += total;
+            System.out.println("Entrou " + total + " pessoa(s) no elevador.");
+
         }
     }
-    public void sai(int quantidade){
-       pessoasNoElevador -= quantidade;
-       capacidadeElevador -= quantidade;
+
+    public void sai(int quantidade) {
+        if ((this.pessoasNoElevador - quantidade) < 0) {
+            System.out.println("Não há pessoas suficientes no elevador");
+        } else {
+            this.pessoasNoElevador -= quantidade;
+
+            System.out.println("Saiu " + quantidade + " pessoa(s) do elevador");
+
+        }
+
     }
 
-    public void sobe(int andares){
+    public void sobe(int andares) {
 
-        if(andarAtual == totalAndares || andares > this.totalAndares){
-            System.out.println("impossivel subir, já esta no último andar");
-        }else{
+        if ((this.andarAtual + andares) > this.totalAndares) {
+            System.out.println("impossivel subir");
+        } else {
             this.andarAtual += andares;
+            System.out.println("Elevador subiu para o " + this.andarAtual + "º andar");
         }
     }
 
-    public void desce(int andares){
-        if(andarAtual == 0){
-            System.out.println("impossivel descer, já está no terreo.");
-        }else{
+    public void desce(int andares) {
+        if ((this.andarAtual - andares) < 0) {
+            System.out.println("impossivel descer");
+        } else {
             this.andarAtual -= andares;
+            System.out.println("Elevador desceu para o " + this.andarAtual + "º andar");
         }
     }
 
--- exercicios/exercicio-04/src/main/java/org/example/Main.java
+++ exercicios/exercicio-04/src/main/java/org/example/Main.java
@@ -4,18 +4,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Elevador e = new Elevador(0,10,5,0);
+        //elevador para industrias stark.
+        Elevador stark = new Elevador(10, 5);
 
-        e.entra(2);
-        e.sobe(2);
-
-        System.out.println(e.andarAtual);
-        e.sobe(8);
-        System.out.println(e.andarAtual);
-
-        System.out.println(e.getPessoasNoElevador());
-        e.entra(6);
-        System.out.println(e.getPessoasNoElevador());
+        stark.entra(5);
+        stark.sobe(9);
+        stark.sai(5);
+        stark.desce(6);
 
 
     }
