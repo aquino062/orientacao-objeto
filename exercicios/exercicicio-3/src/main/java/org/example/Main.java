22741806e16b57a43aef032e86d049eaae51c999
aquino062 <willian_wjunior@hotmail.com>
2023-04-30 00:14:23 -0300

    2.0

--- exercicios/exercicicio-3/src/main/java/org/example/Main.java
+++ exercicios/exercicicio-3/src/main/java/org/example/Main.java
@@ -7,10 +7,20 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        bombaCombustivel bomba = new bombaCombustivel("gasolina",10,500);
+        bombaCombustivel b = new bombaCombustivel("gasolina", 5.50f, 100f);
+
+        b.abastecerLitro(10);
+        System.out.println(b.getQuantidadeCombustivel());
 
 
+        b.alterarCombustivel("alcool");
+        b.alterarQntdCombustivelBomba(500);
+        b.alterarValor(6.00f);
 
+        b.abastecerLitro(50);
+        System.out.println(b.getQuantidadeCombustivel());
+        b.abastecerValor(100);
+        System.out.println(b.getQuantidadeCombustivel());
 
 
     }
--- exercicios/exercicicio-3/src/main/java/org/example/bombaCombustivel.java
+++ exercicios/exercicicio-3/src/main/java/org/example/bombaCombustivel.java
@@ -6,51 +6,50 @@ public class bombaCombustivel {
     private float valorPorLitro;
     private float quantidadeCombustivel;
 
-
-    public void abastecerValor() {
-
-    }
-        public float abastecerLitro ( float qntdLitros){
-            float valorTotal = qntdLitros * this.valorPorLitro;
-            if (qntdLitros > this.quantidadeCombustivel) {
-                System.out.println("Não há combustível suficiente na bomba!");
-                return 0;
-            }
-            this.quantidadeCombustivel -= qntdLitros;
-            return valorTotal;
-        }
-
-
-        public void alterarValor ( float valor){
-            this.valorPorLitro = valor;
-        }
-
-        public void alterarCombustivel (String tipo){
-            this.tipoCombustivel = tipo;
-        }
-
-        public void alterarQntdCombustivelBomba ( float total){
-            this.quantidadeCombustivel = total;
-        }
-
-    public bombaCombustivel(String tipoCombustivel, float valorPorLitro, float quantidadeCombustivel){
-            this.tipoCombustivel = tipoCombustivel;
-            this.valorPorLitro = valorPorLitro;
-            this.quantidadeCombustivel = quantidadeCombustivel;
-        }
-
-        public String getTipoCombustivel () {
-            return tipoCombustivel;
-        }
-
-        public float getValorPorLitro () {
-            return valorPorLitro;
-        }
-
-        public float getQuantidadeCombustivel () {
-            return quantidadeCombustivel;
-        }
-
+    public bombaCombustivel(String tipoCombustivel, float valorPorLitro, float quantidadeCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+        this.valorPorLitro = valorPorLitro;
+        this.quantidadeCombustivel = quantidadeCombustivel;
     }
 
+    public void abastecerValor(float valor) {
+        float qntsLitros = valor / valorPorLitro;
+        if (valor > quantidadeCombustivel) {
+            System.out.println("Não há combustivel suficiente na bomba.");
+        } else {
+            System.out.println("voce abasteceu " + qntsLitros + " litros de " + tipoCombustivel);
+            this.quantidadeCombustivel -= qntsLitros;
 
+        }
+    }
+
+    public void abastecerLitro(float totalLitros) {
+
+        float totalApagar = valorPorLitro * totalLitros;
+        if (totalLitros > quantidadeCombustivel) {
+            System.out.println("Não há combustivel suficiente na bomba.");
+
+        } else {
+            System.out.println("voce abasteceu " + totalLitros + " litros de " + tipoCombustivel);
+            this.quantidadeCombustivel -= totalLitros;
+        }
+    }
+
+    public void alterarValor(float valor) {
+        this.valorPorLitro = valor;
+    }
+
+    public void alterarCombustivel(String tipo) {
+        this.tipoCombustivel = tipo;
+    }
+
+    public void alterarQntdCombustivelBomba(float total) {
+        this.quantidadeCombustivel = total;
+    }
+
+    public float getQuantidadeCombustivel() {
+
+        System.out.println("litros Disponiveis na bomba:");
+        return quantidadeCombustivel;
+    }
+}
