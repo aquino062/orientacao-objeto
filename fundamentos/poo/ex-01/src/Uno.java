461f025407c93729d6ae7ec7af1c8eef7924ed38
willian aquino <willian_wjunior@hotmail.com>
2023-05-19 12:07:26 -0300

    .

---
+++ fundamentos/poo/ex-01/src/Uno.java
@@ -0,0 +1,11 @@
+public class Uno {
+    public static void main(String[] args) {
+        Carro uno = new Carro(1992,"Branco");
+
+        System.out.println("Carro: uno");
+        System.out.println("Ano: " + uno.ano);
+        System.out.println("Cor: " + uno.cor);
+        uno.ligar();
+        uno.acelerar();
+    }
+}
