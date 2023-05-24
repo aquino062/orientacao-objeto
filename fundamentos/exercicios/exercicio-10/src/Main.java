28ab8520600594af095eb03dc60877fb1465a7ac
willian aquino <willian_wjunior@hotmail.com>
2023-05-24 19:45:39 -0300

    .

---
+++ fundamentos/exercicios/exercicio-10/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lançamento do DADO");
+        System.out.println("--------------------");
+        char opcao = 'S';
+
+        do{
+            System.out.println("Deseja lançar o dado? S/N (SIM ou Não )");
+            opcao = scanner.next().toUpperCase().charAt(0);
+            int dado = (int) (Math.random() * 6 + 1);
+            System.out.println("Face: "+ dado);
+        }while(opcao == 'S');
+
+    }
+}
